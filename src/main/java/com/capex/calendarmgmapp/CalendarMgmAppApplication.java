bf619583afd8dc874b89a4612865897f70ed0502
Dinesh Kudva <dineshkudva12@gmail.com>
2023-03-15 19:58:38 +0530

    initial commit

---
+++ src/main/java/com/capex/calendarmgmapp/CalendarMgmAppApplication.java
@@ -0,0 +1,13 @@
+package com.capex.calendarmgmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarMgmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarMgmAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capex/calendarmgmapp/CalendarMgmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capex.calendarmgmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarMgmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
